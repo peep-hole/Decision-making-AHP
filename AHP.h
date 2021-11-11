98c67757c8691707d5c85bbfb9039629c16d2bc0
peep-hole <62802600+peep-hole@users.noreply.github.com>
2021-11-11 07:57:29 +0100

    AHP_class

--- AHP.h
+++ AHP.h
@@ -1,6 +1,19 @@
 #ifndef DECISION_MAKING_AHP_AHP_H
 #define DECISION_MAKING_AHP_AHP_H
 
-void hello();
+#include <string>
+
+using namespace std;
+
+class AHP_solver {
+private:
+    int criteriaMatrixSize;
+    string* criteria;
+    double** criteriaComparisonMatrix;
+    int candidatesCount;
+    string* candidates;
+    double*** candidatesPerCriteriaComparison;
+
+};
 
 #endif //DECISION_MAKING_AHP_AHP_H
